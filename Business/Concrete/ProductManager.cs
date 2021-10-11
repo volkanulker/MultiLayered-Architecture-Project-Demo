972f5978c9361bebfca825ac1f0803b0133cb152
Volkan Ulker <vlkn-ulkr@hotmail.com>
2021-10-11 14:08:26 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            this.productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //Add Business Rules
+            return productDal.GetAll();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InMemoryProductDal inMemoryProductDal = new InMemoryProductDal();
+            ProductManager productManager = new ProductManager(inMemoryProductDal);
+            foreach(var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        
+        }
+    }
+}

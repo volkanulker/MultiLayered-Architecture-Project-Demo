bfdd3658fa4a2e06ddb8d003ce6f1beb8f2b7a87
Volkan Ulker <vlkn-ulkr@hotmail.com>
2021-10-19 21:15:47 +0300

    ProductDetailDto is added.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,14 +10,47 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            EfProductDal efProductDal = new EfProductDal();
-            ProductManager productManager = new ProductManager(efProductDal);
+            //ProductTest();
 
-            //foreach(var product in productManager.GetAllByCategoryId(2))
+            //CategoryTest();
+
+            ProductDetailDtoTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            ICategoryDal efCategoryDal = new EfCategoryDal();
+            CategoryManager categoryManager = new CategoryManager(efCategoryDal);
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+
+            }
+        }
+
+        private static void ProductTest()
+        {
+            //EfProductDal efProductDal = new EfProductDal();
+            //ProductManager productManager = new ProductManager(efProductDal);
+
+            //foreach (var product in productManager.GetAllByCategoryId(2))
             //{
             //    Console.WriteLine(product.ProductName);
             //}
-        
+        }
+
+        private static void ProductDetailDtoTest()
+        {
+            EfProductDal efProductDal = new EfProductDal();
+            ProductManager productManager = new ProductManager(efProductDal);
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine("[" +product.ProductId+ "] " + "["+ product.UnitsInStock+"] "
+                    + "["+product.ProductName+"]"+ "[" + product.CategoryName + "]");
+            }
         }
     }
 }

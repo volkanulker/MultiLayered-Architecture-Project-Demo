dfee66f9b84a70c62416a3b57a965dfda944af9c
Volkan Ulker <vlkn-ulkr@hotmail.com>
2021-11-14 15:52:21 +0300

    JWT objects and helpers are added.

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        // out: variable to out
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            // Disposable Pattern
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                // Creates different key for each user
+                passwordSalt = hmac.Key;
+                // ComputeHash() need password in byte[] format so encode password to byte[]
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+        // Create hash for given password and compare to verify
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                // compare computedHash and passwordHash byte by byte
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            
+
+        }
+
+    }
+}
